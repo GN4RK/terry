562b0b12cee8eceb2a31a616f475ab56a1b4f1a9
Yoann LEONARD <yoann.leonard@gmail.com>
2023-10-02 01:07:59 +0200

    fix error when ping in dm

--- commands/testing/ping.js
+++ commands/testing/ping.js
@@ -8,12 +8,21 @@ module.exports = {
     async execute(interaction) {
         await interaction.reply('Pong!');
 
+        // checking if the interaction is in a server or DM
+        if (!interaction.guild) {
+            var serverName = 'DM';
+            var channelName = 'DM';
+        } else {
+            var serverName = interaction.guild.name;
+            var channelName = interaction.channel.name;
+        }
+
         // adding log
         console.log(JSON.stringify({
             timestamp: getNowFormat(),
             level: "info",
-            server: interaction.guild.name,
-            channel: interaction.channel.name,
+            server: serverName,
+            channel: channelName,
             author: interaction.user.tag,
             message: "Pong!"
         }));
