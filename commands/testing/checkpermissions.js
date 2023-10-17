7010ccd4ca5fb4faa4738cb4d24315871a8f3709
yoann.leonard <yoann.leonard@gmail.com>
2023-10-17 16:30:29 +0200

    add checkpermissions command

---
+++ commands/testing/checkpermissions.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, PermissionsBitField} = require('discord.js');
+const { addLog} = require('../../utils');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('checkpermissions')
+        .setDescription('Check if the bot has all the required permissions in the current channel'),
+    async execute(interaction) {
+
+        // Check if the command is being used in a guild
+        if (!interaction.guild) {
+            await interaction.reply('This command can only be used in a guild.');
+            return;
+        }
+
+        // fetching infos
+        const serverName = interaction.guild.name;
+        const channelName = interaction.channel.name;
+        const authorTag = interaction.user.tag;
+
+        // List of permissions to check
+        const permissionsNeeded = {
+            "ViewChannel"   : PermissionsBitField.Flags.ViewChannel,
+            "SendMessages"  : PermissionsBitField.Flags.SendMessages,
+            "EmbedLinks"    : PermissionsBitField.Flags.EmbedLinks,
+            "AddReactions"  : PermissionsBitField.Flags.AddReactions
+        };
+
+        // Checking channel's permissions one by one
+        for (const [key, value] of Object.entries(permissionsNeeded)) {
+            if (!interaction.channel.permissionsFor(interaction.client.user).has(value)) {
+                await interaction.reply(`The bot does not have the '${key}' permission in this channel.`);
+                addLog("error", serverName, channelName, authorTag, `Check permissions failed: ${key}`);
+                return;
+            }
+        }
+
+        // Replying to the user
+        await interaction.reply('The bot has all the required permissions in this channel.');
+
+        // adding log
+        addLog("info", serverName, channelName, authorTag, "Check permissions success");
+    },
+};
